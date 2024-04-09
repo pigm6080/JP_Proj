3be7d9c55543bf7eecf1c32eebd58bc840126532
YRN <118609415+Kascald@users.noreply.github.com>
2024-04-09 15:23:14 +0900

    Revert "Merge branch 'features' into zz"
    This reverts commit ec7d8b0608967efb368ab402dd1fa3bf15112cea, reversing
    changes made to 3d7e305a6f7b0724685f90344ceab7f87782663a.

---
+++ src/main/java/org/zerock/domain/TripInfoVO.java
@@ -0,0 +1,14 @@
+package org.zerock.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TripInfoVO {
+	private int id;
+    private String hashtag;
+    private String placeName;
+}
